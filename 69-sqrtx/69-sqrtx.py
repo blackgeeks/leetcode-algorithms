f0a2203d1f82661643ea725c87a9e934ea296f1f
Amad Uddin <36585842+blackgeeks@users.noreply.github.com>
2022-05-06 11:21:19 +0500

    Time: 47 ms (66.37%), Space: 13.6 MB (99.98%) - LeetHub

---
+++ 69-sqrtx/69-sqrtx.py
@@ -0,0 +1,29 @@
+class Solution:
+    def mySqrt(self, x: int) -> int:
+        
+        if (x == 0 or x == 1) :
+            return x
+  
+        start = 1
+        end = x  
+        while (start <= end) :
+            mid = (start + end) // 2
+
+            # If x is a perfect square
+            if (mid*mid == x) :
+                return mid
+
+            # Since we need floor, we update
+            # answer when mid*mid is smaller
+            # than x, and move closer to sqrt(x)
+            if (mid * mid < x) :
+                start = mid + 1
+                ans = mid
+
+            else :
+
+                # If mid*mid is greater than x
+                end = mid-1
+
+        return ans
+ 
